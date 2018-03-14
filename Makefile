90c4446814165269e35ae7c5aa288ca35c1eca57
fxst1 <fxst1@localhost.localdomain>
2018-03-14 12:00:31 +0100

    fix

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: fxst1 <fxst1@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/03/12 10:57:23 by fxst1             #+#    #+#              #
-#    Updated: 2018/03/14 11:40:49 by fxst1            ###   ########.fr        #
+#    Updated: 2018/03/14 12:00:25 by fxst1            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 CC = cc
@@ -44,3 +44,8 @@ fclean:
 	@rm -rf $(OTOOL)
 
 re: fclean all
+
+push:
+	git add --all
+	git commit -am "fix"
+	git push
--- src/binary_corrupt.c
+++ src/binary_corrupt.c
@@ -6,7 +6,7 @@
 /*   By: fxst1 <fxst1@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 11:24:08 by fxst1             #+#    #+#             */
-/*   Updated: 2018/03/14 11:39:48 by fxst1            ###   ########.fr       */
+/*   Updated: 2018/03/14 11:59:28 by fxst1            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,12 @@ void			binary_can_read(t_binary *bin, size_t nbytes)
 
 void			binary_is_corrupt(t_binary *bin, void *addr, size_t nbytes)
 {
-	if (bin->size <= (size_t)((addr + nbytes) - (void*)bin->buffer))
+	size_t		start;
+	size_t		end;
+
+	start = (size_t)addr;
+	end = start + nbytes;
+	if (((size_t)bin->buffer) + bin->size <= end)
 	{
 		ft_putstr_fd("Corrupt file !\n", STDERR_FILENO);
 		exit(EXIT_FAILURE);
