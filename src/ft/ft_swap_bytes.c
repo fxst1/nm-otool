d4dae7b8e8a658d968c853551541a039d44e9912
Faustin JACQUEMAIN <fjacquem@e1r4p8.42.fr>
2018-09-01 14:21:40 +0200

    PPC

--- src/ft/ft_swap_bytes.c
+++ src/ft/ft_swap_bytes.c
@@ -6,12 +6,12 @@
 /*   By: fjacquem <fjacquem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 20:20:45 by fjacquem          #+#    #+#             */
-/*   Updated: 2018/05/24 13:08:40 by fjacquem         ###   ########.fr       */
+/*   Updated: 2018/09/01 14:16:17 by fjacquem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int		ft_swap_bytes(unsigned int bytes)
 {
-	bytes = ((bytes << 8) & 0xFF00FF00) | ((bytes >> 8) & 0xFF00FF);
+	bytes = ((bytes << 8) & 0xFF00FF00) | ((bytes >> 8) & 0x00FF00FF);
 	return (bytes << 16) | (bytes >> 16);
 }
